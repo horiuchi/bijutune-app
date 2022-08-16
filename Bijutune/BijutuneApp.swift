af926f478beb4856a58f37acd5aa04933b52df6e
Hiroki Horiuchi <horiuchi.g@gmail.com>
2022-08-16 16:56:35 +0900

    Initial Commit

---
+++ Bijutune/BijutuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BijutuneApp.swift
+//  Bijutune
+//
+//  Created by 堀内浩樹 on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct BijutuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
